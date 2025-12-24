356a12b07cba874e6c3a92d65ee38bb9abdaa02f
codingghoul7 <the.raushankr@gmail.com>
2025-12-25 04:14:38 +0530

    add book and bookreview section in the admin dashboard via route need to improve admin route protection

--- src/books/orders/middleware/verifyAdminToken.js
+++ src/books/orders/middleware/verifyAdminToken.js
@@ -4,9 +4,11 @@ const JWT_SECRET = process.env.JWT_SECRET;
 const verifyAdminToken = (req, res, next) => {
     const token = req.headers['authorization']?.split(' ')[1];
 
+
     if (!token) {
         return res.status(401).json({ message: "Token not Provided. Access Denied" })
     }
+
 
     jwt.verify(token, JWT_SECRET, (err, user) => {
         if (err) {
