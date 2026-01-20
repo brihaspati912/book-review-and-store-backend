f5a9f03b2eb6deda1d1ee895d429ecf4d333a249
Brihaspati <108980953+brihaspati912@users.noreply.github.com>
2026-01-20 11:54:44 +0530

    Fix import path for verifyAdminToken middleware

--- src/books/books-routes.js
+++ src/books/books-routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { model } = require('mongoose');
 const router = express.Router();
 const Book = require("./BookModel");
-const { verifyAdminToken } = require('../books/orders/middleware/VerifyAdminToken');
+const { verifyAdminToken } = require('../books/orders/middleware/verifyAdminToken');
 const { postABook, getAllBooks, getABook, updateABook, deleteABook } = require('./book.controller');
 
 //get=when get back data from backend to frontend
